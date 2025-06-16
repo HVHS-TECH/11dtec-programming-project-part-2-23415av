3d431283ff8edc8c266f2a114d30f935d6f61554
23415av <23415av@hvhs.school.nz>
2025-06-16 21:53:39 +0000

    Jun 16, 2025, 9:53 PM

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ Term 2
 
 const listOfGenres = ['Choose Here', 'Something Weird', 'Something Alternative', 'Something Older', 'Something Pop', 'Something Parody', 'Something Emotional'];
 let index = listOfGenres.indexOf('Something Weird');
-const listOfCds = ['N/A', 'Everything is a Lot - Will Wood', 'Eureka - Mother Mother', 'Diamond Dogs - David Bowie', 'Reputation - Taylor Swift', 'Bad Hair Day - “Weird Al” Yankovic', 'The Black Parade - My Chemical Romance'];
-const cdPrices = ['N/A', '$3/day', '$3/day', ' $5/day', ' $5/day', ' $4/day', '$3.50/day'];
+listOfCds = ['N/A', 'Everything is a Lot - Will Wood', 'Eureka - Mother Mother', 'Diamond Dogs - David Bowie', 'Reputation - Taylor Swift', 'Bad Hair Day - “Weird Al” Yankovic', 'The Black Parade - My Chemical Romance'];
+cdPrices = ['N/A', '$3/day', '$3/day', ' $5/day', ' $5/day', ' $4/day', '$3.50/day'];
 /*************************
 Main Code
 *************************/
@@ -36,7 +36,7 @@ function genreDropdown() {
 genreDropdown();
 
 function yourCd (){
-alert(genreSelect.value)
+alert(genreSelect.indexOf)
     /*var cd = "The CD you have selected is ";
     for (i = 0; i < listOfCds.length; i++) {
         if(listOfCds[i] <= listOfGenres) {
