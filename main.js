3db8f6207119f7a079c8fe8f5f996d3ab10af902
23415av <23415av@hvhs.school.nz>
2025-06-16 21:15:13 +0000

    Jun 16, 2025, 9:15 PM

--- main.js
+++ main.js
@@ -6,8 +6,9 @@ Term 2
 
 //Variables
 
-
-
+listOfGenres = ['Choose Here', 'Something Weird', 'Something Alternative', 'Something Older', 'Something Pop', 'Something Parody', 'Something Emotional'];
+listOfCds = ['N/A', 'Everything is a Lot - Will Wood', 'Eureka - Mother Mother', 'Diamond Dogs - David Bowie', 'Reputation - Taylor Swift', 'Bad Hair Day - “Weird Al” Yankovic', 'The Black Parade - My Chemical Romance'];
+cdPrices = ['N/A', '$3/day', 'Eureka - Mother Mother', 'Diamond Dogs - David Bowie', 'Reputation - Taylor Swift', 'Bad Hair Day - “Weird Al” Yankovic', 'The Black Parade - My Chemical Romance'];
 /*************************
 Main Code
 *************************/
@@ -24,10 +25,10 @@ function getNameInput (){
 }
 
 
-listOfCds = ['Choose Here', 'Something Weird', 'Something Alternative', 'Something Older', 'Something Pop', 'Something Parody', 'Something Emotional'];
+
 function genreDropdown() {
     for (i = 0; i < listOfCds.length; i++) {
-        var newOption = '<option value="' + listOfCds[i] + '">' + listOfCds[i] + '</option>';
+        var newOption = '<option value="' + listOfGenres[i] + '">' + listOfGenres[i] + '</option>';
         genreSelect.innerHTML += newOption;
     }
 }
