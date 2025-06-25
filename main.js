3d1d58bc9b312e97ae5c510b3411acc479d40412
23415av <23415av@hvhs.school.nz>
2025-06-25 00:28:24 +0000

    follow the list of things to do

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ Term 2
 *************************/
 
 //Variables
-
 const listOfGenres = ['Choose Here', 'Something Weird', 'Something Alternative', 'Something Older', 'Something Pop', 'Something Parody', 'Something Emotional'];
 let index = listOfGenres.indexOf('Something Weird');
 listOfCds = ['N/A', 'Everything is a Lot - Will Wood', 'Eureka - Mother Mother', 'Diamond Dogs - David Bowie', 'Reputation - Taylor Swift', 'Bad Hair Day - “Weird Al” Yankovic', 'The Black Parade - My Chemical Romance'];
@@ -19,14 +18,19 @@ Main Code
 /*************************
 Functions
 *************************/
-function getNameInput (){
+/*
+This function serves the purpose of being able to collect the users name.
+It will then display the users name in a welcome message.
+*/
+function getNameInput() {
     var message = "Welome to Cool Robs CDs for rent ";
-    message  = message + input_name.value;
+    message = message + input_name.value;
     welcomeMessage.innerHTML = message;
 }
-
-
-
+/*
+This function serves the purpose of being able to allow the user to choose a genre.
+It will then be used to proceed with rental information.
+*/
 function genreDropdown() {
     for (i = 0; i < listOfCds.length; i++) {
         var newOption = '<option value="' + listOfGenres[i] + '">' + listOfGenres[i] + '</option>';
@@ -34,22 +38,25 @@ function genreDropdown() {
     }
 }
 genreDropdown();
-
-function yourCd (){
-alert(genreSelect.indexOf)
-    /*var cd = "The CD you have selected is ";
-    for (i = 0; i < listOfCds.length; i++) {
-        if(listOfCds[i] <= listOfGenres) {
-            cd  = cd + listOfCds[i];
-            cdName.innerHTML = cd;
-        }
-    }
-
-        var price = "This will cost you ";
-    for (i = 0; i < cdPrices.length; i++) {
-        if(cdPrices[i] <= listOfGenres) {
-            price  = price + listOfCds[i];
-            dailyPrice.innerHTML = price;
-        }
-    }*/
+/*
+This function serves the purpose of being able to show the users CD and pricing per day.
+It will then display the users choice and price with a message underneath.
+*/
+function yourCd() {
+    var cd = "The CD you have selected is ";
+    cd = cd + listOfCds[listOfGenres.indexOf(genreSelect.value)];
+    cdName.innerHTML = cd;
+    var price = "This will cost you ";
+    price = price + cdPrices[listOfGenres.indexOf(genreSelect.value)];
+    dailyPrice.innerHTML = price;
 }
+/*
+What I need to do now so I do not forget.
+Figure out how many days the user wishes to rent the cd.
+Make sure it is valid. Validating your input slide 4
+Have a max and min for days. 1-10
+Use the math ability to display how much it will cost them for that many days for that cd. base number x dayX
+Have one underneath that shows what the price would be with the additional charge of a cd player rent. base number + 20 x dayX
+Add ones for bond payment if time.
+Then work on website look if time.
+*/
